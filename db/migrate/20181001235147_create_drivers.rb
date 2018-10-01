8517235806ddbde871804029ef33c3290e742fd6
Kay <kayxn23@gmail.com>
2018-10-01 16:52:06 -0700

    created Driver model

---
+++ db/migrate/20181001235147_create_drivers.rb
@@ -0,0 +1,11 @@
+class CreateDrivers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :drivers do |t|
+      t.integer :id
+      t.string :name
+      t.string :vin
+
+      t.timestamps
+    end
+  end
+end
