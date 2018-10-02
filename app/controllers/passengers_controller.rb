ce598a481ac6ce86c0dbd2aab371557dbdfbb457
jackie <jacquelyn.cheng@me.com>
2018-10-02 10:52:22 -0700

    Added all Passenger restful controller methods

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -8,17 +8,40 @@ class PassengersController < ApplicationController
   end
 
   def new
+    @passenger = Passenger.new
   end
 
   def create
+    passenger = Passenger.new(passenger_params) #instantiate a new book
+
+    if passenger.save
+      redirect_to passenger_path(passenger.id)
+    else
+      render :new
+    end
   end
 
   def edit
+    @passenger = Passenger.find_by(id: params[:id].to_i)
   end
 
   def update
+    passenger = Passenger.find_by(id: params[:id].to_i)
+    passenger.update(passenger_params)
+
+    redirect_to passenger_path(passenger.id)
   end
 
   def destroy
+    passenger_to_delete = Passenger.find_by(id: params[:id].to_i)
+    passenger_to_delete.destroy
+
+    redirect_to passengers_path
   end
+
+  private
+
+    def passenger_params
+      return params.require(:passenger).permit(:id, :name, :phone_num)
+    end
 end
