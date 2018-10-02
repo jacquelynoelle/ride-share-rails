0f8c4e1c29be406474a05b3339f4a16a90a4864a
jackie <jacquelyn.cheng@me.com>
2018-10-02 10:35:49 -0700

    Implemented Passenger#index Passenger#show and super basic nav

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,8 +1,10 @@
 class PassengersController < ApplicationController
   def index
+    @passengers = Passenger.all.order(:name)
   end
 
   def show
+    @passenger = Passenger.find_by(id: params[:id].to_i)
   end
 
   def new
