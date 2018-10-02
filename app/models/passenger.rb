d5d2a342a747de053891ac2f997c33b07029a36c
jackie <jacquelyn.cheng@me.com>
2018-10-02 15:28:56 -0700

    Added total_spent method to Passenger model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -4,4 +4,10 @@ class Passenger < ApplicationRecord
   def list_trips
     return self.trips.where(passenger_id: id)
   end
+
+  def total_spent
+    rides = list_trips
+    spending = rides.map { |trip| (trip.cost * 0.01) }
+    return spending.sum.round(2)
+  end
 end
