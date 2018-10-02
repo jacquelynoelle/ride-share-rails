692f0897f9463bbd9077f331304a5ffc519676fa
Kay <kayxn23@gmail.com>
2018-10-02 15:19:30 -0700

    created total_earnings method in the Driver model and updated the view accordingly

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,16 @@
 class Driver < ApplicationRecord
-  has_many :trips 
+  has_many :trips
+
+  def list_trips
+    return self.trips.where(driver_id: id)
+  end
+
+  def total_earnings
+    driven_trips = list_trips
+    raw_earnings = driven_trips.map { |trip| (trip.cost * 0.01 - 1.65)}
+    total_earnings = raw_earnings.sum * 0.8
+    return total_earnings.round(2)
+  end
+
+
 end
