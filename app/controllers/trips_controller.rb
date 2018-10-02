cad217a9507d96f68b203578d082d1bd0eb5be39
jackie <jacquelyn.cheng@me.com>
2018-10-02 10:53:41 -0700

    Added restful Trip controller methods

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,22 +1,47 @@
 class TripsController < ApplicationController
   def index
+    @trip = Trips.all.order(:name)
   end
 
   def show
+    @trip = Trips.find_by(id: params[:id].to_i)
   end
 
   def new
+    @trip = Trips.new
   end
 
   def create
+    trip = Trips.new(trip_params) #instantiate a new book
+
+    if trip.save
+      redirect_to trip_path(trip.id)
+    else
+      render :new
+    end
   end
 
   def edit
+    @trip = Trips.find_by(id: params[:id].to_i)
   end
 
   def update
+    trip = Trips.find_by(id: params[:id].to_i)
+    trip.update(trip_params)
+
+    redirect_to trip_path(trip.id)
   end
 
   def destroy
+    trip_to_delete = Trips.find_by(id: params[:id].to_i)
+    trip_to_delete.destroy
+
+    redirect_to trips_path
   end
+
+  private
+
+    def trip_params
+      return params.require(:trip).permit(:id, :name, :phone_num)
+    end
 end
