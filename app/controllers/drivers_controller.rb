5b7bec826a07d248e2eee208f9ecfb6a3d08513a
Kay <kayxn23@gmail.com>
2018-10-02 10:37:53 -0700

    modifed Driver view in edit, index and created partial, and created index show edit and delete controller methods

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,8 +1,15 @@
 class DriversController < ApplicationController
   def index
+    @drivers = Driver.all.order(:name)
   end
 
   def show
+    id = params[:id].to_i
+    @driver = Driver.find_by(id: id)
+
+    if @driver.nil?
+      render :notfound, status: :not_found
+    end
   end
 
   def new
@@ -12,11 +19,15 @@ class DriversController < ApplicationController
   end
 
   def edit
+    @driver = Driver.find(params[:id].to_i)
   end
 
   def update
   end
 
   def destroy
+    driver = Driver.find_by(id: params[:id].to_i)
+    driver.destroy
+    redirect_to root_path
   end
 end
